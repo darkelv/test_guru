cc6dd6c93a49476d653abc10a50ab5be7a190e94
darkelv <darkelv777@yandex.ru>
2020-03-01 15:47:05 +0300

    fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,38 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[create new]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def show
-    question = Question.find(params[:id])
-    render locals: { question: question }
+    render locals: { question: @question }
   end
 
   def new; end
 
   def create
-    @test.questions.create(question_params)
-
-    redirect_to test_path(@test.id)
+    question = @test.questions.new(question_params)
+    if question.save
+      redirect_to question_path(question)
+    else
+      redirect_to new_test_question_path(@test)
+    end
   end
 
   def destroy
-    question = Question.find(params[:id])
-    question.destroy
+    @question.destroy
 
-    redirect_to question.test
+    redirect_to @question.test
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
