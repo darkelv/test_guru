c57c2518d1f624f20285ffa55d252ae7f1b75094
darkelv <darkelv777@yandex.ru>
2020-03-10 09:54:59 +0300

    add_email_index_to_db

---
+++ db/migrate/20200310065320_add_email_index_and_last_name_to_user.rb
@@ -0,0 +1,5 @@
+class AddEmailIndexAndLastNameToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_092804) do
+ActiveRecord::Schema.define(version: 2020_03_10_065320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_03_07_092804) do
     t.datetime "updated_at", null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name", "email"], name: "index_users_on_name_and_email", unique: true
   end
 
