6c98050987a131371881e9f56938356dd0e54bdc
darkelv <darkelv777@yandex.ru>
2020-03-18 17:13:47 +0300

    lesson_14

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :passed_tests
   has_many :tests, through: :passed_tests
   has_many :author_tests, class_name: "Test"
+  has_many :gists
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
